87cee893b91053c388f84fbd1245f3a9b38715f1
miasumon <miasumon.ru@gmail.com>
2024-05-04 11:58:49 +0600

    added services data to the mongodb and created services api

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const servicesCollection = client.db("carsDoctor").collection("services")
+
+        app.get('/services', async(req, res)=> {
+            const cursor = servicesCollection.find();
+
+            const result = await cursor.toArray()
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
